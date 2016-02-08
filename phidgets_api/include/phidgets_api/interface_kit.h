baff5f0b054ce3ffbf4276a792ccd971d94d3935
Dorian Goepp <dorian.goepp@inria.fr>
2016-02-08 18:19:33 +0100

    improve comments

--- phidgets_api/include/phidgets_api/interface_kit.h
+++ phidgets_api/include/phidgets_api/interface_kit.h
@@ -16,26 +16,25 @@ namespace phidgets {
 
         InterfaceKit();
 
-        // set the output state of one or more chanels
+        // Get the output state of one or more chanels
         bool get_state(int index);
         std::vector<bool> get_states(std::vector<int> &indices);
-        // to be compatible to how ROS handle types in messages and services
+        // to be compatible with the types in ROS messages and services
         std::vector<uint16_t> get_states(std::vector<uint16_t> &indices);
 
-        // get the state of one or more (digital) channels
+        // Set the state of one or more (digital) channels
         void set_state(int index, bool state);
         void set_states(std::vector<int> &indices, std::vector<bool> &states);
-        // to be compatible to how ROS handle types in messages and services
+        // to be compatible with the types in ROS messages and services
         void set_states(std::vector<uint16_t> &indices, std::vector<uint16_t> &states);
 
     protected:
 
         CPhidgetInterfaceKitHandle _interface_kit_handle;
 
-        // store the state of each output, indexed by their output index
-        // provided by Phidgets
+        // store the state of each output
         std::vector<bool> _digital_output_state;
-
+        // called by output_change_callback to update _digital_output_state
         void update_output_state(int index, bool state);
 
     private:
