8e40b25e02b6eff08948bbac48b2276663e83580
Dorian Goepp <dorian.goepp@inria.fr>
2016-02-15 17:16:50 +0100

    change node's name to phidgets_ik

--- phidgets_interface_kit/src/main.cpp
+++ phidgets_interface_kit/src/main.cpp
@@ -2,8 +2,8 @@
 
 int main(int argc, char **argv)
 {
-  ros::init (argc, argv, "PhidgetsIK");
-  ros::NodeHandle nh;
+  ros::init (argc, argv, "phidgets_ik");
+  ros::NodeHandle nh("~");
 
   // Connect to the Interface Kit and advertise services
   phidgets::InterfaceKitNode interface_kit(nh);
